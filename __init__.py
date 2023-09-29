5b9736371379f5dcad3ef324c69ecb7b0b456116
Linwei Mou <moulinwe@msu.edu>
2023-09-29 14:23:18 -0400

    host site

---
+++ __init__.py
@@ -0,0 +1,10 @@
+"""several functions come from the CMSE890-402 class.
+
+Modules exported by this package:
+
+- `my_adder(a, b, c)` - Returns the sum of 3 numbers.
+- `my_thermo_stat(temp, desired_temp)` - Returns the status of the thermostat.
+- `have_digits(s)` - Returns any digits in the input string.
+- `area_of_rectangle(width, height)` - Returns the area of rectangle.
+- `perimeter_of_rectangle(width, height)` - Returns the perimeter of rectangle.
+"""
